8c6397c23f8fd4e5b29f193fe31a84579dd6721d
Qira-Qira <nawafariq3@gmail.com>
2026-05-20 09:30:35 +0700

    Membuat method dari referensi subclass

--- Transaksi.java
+++ Transaksi.java
@@ -8,4 +8,9 @@ public class Transaksi {
     public void tampilkIdentitas(){
         System.out.println("Petugas : " + namaPetugas);
     }
+
+    public void prosesTransaksi(Kendaraan kendaraan, int hari, String namaPelanggan) {
+        
+    }
+
 }
