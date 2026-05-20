e4b7a84dd135a6169b4345e46670ba87f3569fce
Qira-Qira <nawafariq3@gmail.com>
2026-05-20 09:17:56 +0700

    Membuat Contructor & super class dari kendaraan

--- Motor.java
+++ Motor.java
@@ -1,3 +1,8 @@
 public class Motor extends Kendaraan {
     private String jenisMotor;
+
+    public Motor(String nama, String kondisi, double tarifHarian, int unitKendaraan, String jenisMotor) {
+        super(nama, kondisi, tarifHarian, unitKendaraan);
+        this.jenisMotor = jenisMotor;
+    }
 }
