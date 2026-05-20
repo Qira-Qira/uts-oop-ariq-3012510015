059ad838c766f8aa8db45e0b179189dc2de09a5d
Qira-Qira <nawafariq3@gmail.com>
2026-05-20 09:13:00 +0700

    Membuat Constructor dengan super class dari kendaraan

--- Mobil.java
+++ Mobil.java
@@ -1,4 +1,10 @@
 public class Mobil extends Kendaraan {
     private int jumlahKursi;
     private String transmisi;
+
+     public Mobil(String nama, String kondisi, double tarifHarian, int unitKendaraan, int jumlahKursi, String transmisi) {
+        super(nama, kondisi, tarifHarian, unitKendaraan);
+        this.jumlahKursi = jumlahKursi;
+        this.transmisi = transmisi;
+    }
 }
