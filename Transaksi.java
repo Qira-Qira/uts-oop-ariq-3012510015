7e44d64bff33b02a9ed55fdf54fc7c9c30ee1f0a
Qira-Qira <nawafariq3@gmail.com>
2026-05-20 09:34:57 +0700

    Merapikan Kode

--- Transaksi.java
+++ Transaksi.java
@@ -1,11 +1,11 @@
 public class Transaksi {
     private String namaPetugas;
 
-    public Transaksi(String namaPetugas){
+    public Transaksi(String namaPetugas) {
         this.namaPetugas = namaPetugas;
     }
 
-    public void tampilkIdentitas(){
+    public void tampilkIdentitas() {
         System.out.println("Petugas : " + namaPetugas);
     }
 
@@ -30,15 +30,15 @@ public class Transaksi {
             System.out.println("Status Transaksi : Gagal");
             System.out.println("Alasan : Kondisi kendaraan tidak layak digunakan");
             return;
-        } else{
-            
-        kendaraan.sewa(hari);
-        double total = kendaraan.hitungHargaAkhir(hari);
+        } else {
 
-        System.out.println("Status Transaksi : Berhasil");
-        System.out.println("Keterangan : " + kendaraan.tipeKendaraan() + " Berhasil disewa");
-        System.out.println("Total Biaya : Rp" + (long) total + ".0");
-        System.out.println("Sisa Unit : " + kendaraan.getUnitKendaraan());
+            kendaraan.sewa(hari);
+            double total = kendaraan.hitungHargaAkhir(hari);
+
+            System.out.println("Status Transaksi : Berhasil");
+            System.out.println("Keterangan : " + kendaraan.tipeKendaraan() + " Berhasil disewa");
+            System.out.println("Total Biaya : Rp" + (long) total + ".0");
+            System.out.println("Sisa Unit : " + kendaraan.getUnitKendaraan());
         }
 
     }
