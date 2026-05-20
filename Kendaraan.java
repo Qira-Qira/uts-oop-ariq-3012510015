38889a8f5e0918645bb84655be8bd3331b774415
Qira-Qira <nawafariq3@gmail.com>
2026-05-20 08:36:01 +0700

    Membuat Constructor

--- Kendaraan.java
+++ Kendaraan.java
@@ -3,4 +3,11 @@ public abstract class Kendaraan{
     private String kondisi;
     private double tarifHarian;
     private int unitKendaraan;
+
+    public Kendaraan(String nama, String kondisi, double tarifHarifan, int unitKendaraan){
+        this.nama = nama;
+        this.kondisi = kondisi;
+        this.tarifHarian = tarifHarifan;
+        this.unitKendaraan = unitKendaraan;
+    }
 }
