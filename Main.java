1cb737cd847c0e8414d29cb9f4b60ea3cc9d37ca
Qira-Qira <nawafariq3@gmail.com>
2026-05-20 09:39:47 +0700

    Membuat Upcasting

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
 public class Main {
-    
+    public static void main(String args[]){
+        Kendaraan avanza = new Mobil("Toyota Avanza", "Baik", 385000, 3, 7, "Manual");
+        Kendaraan beat = new Motor("Honda Beat", "Baik", 80000, 5, "Matic");
+        Kendaraan brio = new Mobil("Honda Brio", "Baik", 320000, 0, 5, "Matic");
+        Kendaraan r15 = new Motor("Yamaha R15", "Rusak", 200000, 2, "Sport");
+    }
 }
